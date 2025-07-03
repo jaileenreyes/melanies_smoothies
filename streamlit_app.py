a01cf14cf589cc163b983599e96b69e81b3563c7
jaileenreyes <122712972+jaileenreyes@users.noreply.github.com>
2025-07-03 19:36:14 +0000

    changed line 17 from "session = get_active_session()" to """

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,6 @@ name_on_order = st.text_input('Name on smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
 
 
-session = get_active_session()
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'))
