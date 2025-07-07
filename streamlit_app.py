291b1d902722d304513c822a661af946e4465fe9
jaileenreyes <122712972+jaileenreyes@users.noreply.github.com>
2025-07-07 10:08:25 -0500

    Update streamlit_app.py
    changes given by snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,10 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+' '
+        smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/watermelon")
+#st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
         
     #st.write(ingredients_string);
    
@@ -44,6 +48,6 @@ if ingredients_list:
 # new section to display smoothiefroot nutrition information
 import requests
 smoothiefroot_response = requests.get("http://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
