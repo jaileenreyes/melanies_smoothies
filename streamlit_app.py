feb9081a51f080c041e8f578f31ed1afd7f2057a
jaileenreyes <122712972+jaileenreyes@users.noreply.github.com>
2025-07-07 15:29:44 -0500

    Update streamlit_app.py
    fixed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 #st.stop()
 
 #convert the snowspark dataframe to a pandas dataframe so we can use the LOC function
-pd = pd.dataframe.to_pandas()
+pd_df= my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop()
                                                                       
